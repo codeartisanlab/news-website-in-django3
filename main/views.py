e2a3acfcbdd929ebcc39844768182df36fb7bb7d
unknown <codeartisanlab2607@gmail.com>
2020-12-09 08:29:31 +0530

    updated

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import News, Category
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import News, Category,Comment
 def home(request):
     first_news=News.objects.first()
     three_news=News.objects.all()[1:3]
@@ -20,11 +21,24 @@ def all_news(request):
 # Detail Page
 def detail(request,id):
     news=News.objects.get(pk=id)
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        comment=request.POST['message']
+        Comment.objects.create(
+            news=news,
+            name=name,
+            email=email,
+            comment=comment
+        )
+        messages.success(request,'Comment submitted but in moderation mode.')
     category=Category.objects.get(id=news.category.id)
     rel_news=News.objects.filter(category=category).exclude(id=id)
+    comments=Comment.objects.filter(news=news,status=True).order_by('-id')
     return render(request,'detail.html',{
         'news':news,
-        'related_news':rel_news
+        'related_news':rel_news,
+        'comments':comments
     })
 
 # Fetch all category
